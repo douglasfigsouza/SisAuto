d1dd3c15f4d5d5f6aa7df13b622edb202b7c5d36
Douglas da Mata <douglasfigsouza@gmail.com>
2017-07-18 09:55:03 -0300

    Add project files.

---
+++ SisAuto/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SisAuto.MvcApplication" Language="C#" %>
---
+++ SisAuto/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SisAuto.Startup))]
+namespace SisAuto
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
